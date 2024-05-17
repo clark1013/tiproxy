53913fbc5e1180f81a796ab82b516135bb27aea0
ClarkWen <jiazhi.wen@pingcap.com>
2024-05-17 14:13:10 +0800

    introduce zero backend mode

--- pkg/manager/router/router_score.go
+++ pkg/manager/router/router_score.go
@@ -33,14 +33,17 @@ type ScoreBasedRouter struct {
 	backends     *glist.List[*backendWrapper]
 	observeError error
 	// Only store the version of a random backend, so the client may see a wrong version when backends are upgrading.
-	serverVersion string
+	serverVersion     string
+	enableZeroBackend bool
+	inZeroBackendMode bool
 }
 
 // NewScoreBasedRouter creates a ScoreBasedRouter.
-func NewScoreBasedRouter(logger *zap.Logger) *ScoreBasedRouter {
+func NewScoreBasedRouter(logger *zap.Logger, enableZeroBackend bool) *ScoreBasedRouter {
 	return &ScoreBasedRouter{
-		logger:   logger,
-		backends: glist.New[*backendWrapper](),
+		logger:            logger,
+		backends:          glist.New[*backendWrapper](),
+		enableZeroBackend: enableZeroBackend,
 	}
 }
 
@@ -314,9 +317,32 @@ func (router *ScoreBasedRouter) OnBackendChanged(backends map[string]*BackendHea
 			backend.setHealth(*health)
 			router.adjustBackendList(be, true)
 		}
+		if health.Status == StatusHealthy {
+			router.inZeroBackendMode = false
+		}
 	}
+	router.logger.Info(
+		"debug backend change",
+		zap.Int("len", router.backends.Len()),
+		zap.Bool("zero_backend", router.inZeroBackendMode),
+		zap.Any("backends", router.backends),
+	)
 	if len(backends) > 0 {
 		router.updateServerVersion()
+	}
+	if router.enableZeroBackend && router.backends.Len() == 1 {
+		be := router.backends.Front()
+		backend := be.Value
+		if !backend.Healthy() {
+			if !router.inZeroBackendMode {
+				router.logger.Info("last backend become unhealthy, notify connections to save session")
+				for ele := backend.connList.Front(); ele != nil; ele = ele.Next() {
+					conn := ele.Value
+					conn.SaveSession()
+				}
+			}
+			router.inZeroBackendMode = true
+		}
 	}
 }
 
@@ -430,6 +456,10 @@ func (router *ScoreBasedRouter) ServerVersion() string {
 	version := router.serverVersion
 	router.Unlock()
 	return version
+}
+
+func (router *ScoreBasedRouter) InZeroBackendMode() bool {
+	return router.inZeroBackendMode
 }
 
 // Close implements Router.Close interface.
